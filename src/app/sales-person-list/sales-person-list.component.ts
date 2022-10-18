c5d0a464e17a7f4af34d300e5e66eb4a175b79b5
heysungj <linjing0525@gmail.com>
2022-10-18 11:58:07 -0400

    create an array of salesPerson objects

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -6,6 +6,13 @@ import { SalesPerson } from './sales-person';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+// create an array of objects
+    salesPersonList: SalesPerson[] = [
+      new SalesPerson('Anup', 'Kumar', 'anup.kumar@gmail.com', 5000),
+      new SalesPerson('John', 'Dow', 'john.dow@gmail.com', 5000),
+      new SalesPerson('clark', 'king', 'clark.king@gmail.com', 4000),
+      new SalesPerson('Mai', 'Truong', 'mai.troung@gmail.com', 6000),
+    ];
 
   constructor() { }
 
